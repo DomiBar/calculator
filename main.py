66b9a1d90e712924aa885e38f3d9b7b5d86b6f8c
Dominik Baran <dominik.baran1992@gmail.com>
2020-06-10 22:26:38 +0200

    Add operations.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import operations
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
 operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
@@ -19,17 +20,17 @@ except:
     exit()
 
 variable1=float(variable1)
-variable1=float(variable2)
+variable2=float(variable2)
 
 if operation==1:
-    logging.debug(f"Dodaję {variable1} i {variable2}")
-    print(f"Wynik to {variable1+variable2}")
+    operations.add(variable1,variable2)
 elif operation==2:
     logging.debug(f"Odejmuję {variable2} od {variable1}")
     print(f"Wynik to {variable1-variable2}")
 elif operation==3:
-    logging.debug(f"Mnożę {variable1} i {variable2}")
-    print(f"Wynik to {variable1*variable2}")
+    operations.multiply(variable1,variable2)
 elif operation==4:
     logging.debug(f"Dzielę {variable1} przez {variable2}")
     print(f"Wynik to {variable1/variable2}")
+
+
