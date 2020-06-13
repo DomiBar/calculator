4ef722aa0d8ad084636d5d82fae1da838d161900
Dominik Baran <dominik.baran1992@gmail.com>
2020-06-13 12:09:28 +0200

    Clean code

--- main.py
+++ main.py
@@ -5,22 +5,17 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filen
 operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
 variable1 = input("Podaj składnik 1. ")
 try:
-    float(variable1)
-    res=True
+    variable1=float(variable1)
 except:
     print("Podany składnik nie jest liczbą")
     exit()
 
 variable2 = input("Podaj składnik 2. ")
 try:
-    float(variable2)
-    res=True
+    variable2=float(variable2)
 except:
     print("Podany składnik nie jest liczbą")
     exit()
-
-variable1=float(variable1)
-variable2=float(variable2)
 
 if operation==1:
     operations.add(variable1,variable2)
--- operations.py
+++ operations.py
@@ -1,44 +1,38 @@
 import logging
 
 def add(variable1,variable2):
-    res=None
     components=[variable1,variable2]
-    i=3
+    counter=3
     result=0
     res=input("Czy dodać kolejny składnik [y]")
     while res == 'y':
-        component=input(f"Podaj składnik {i}: ")
+        component=input(f"Podaj składnik {counter}: ")
         try:
-            float(component)
-            res=True
+            components.append(float(component))
         except:
             print("Podany składnik nie jest liczbą")
             exit()
-        components.append(float(component))
-        i+=1
-        res=input("Czy dodać kolejny składnik [Y]")
+        counter+=1
+        res=input("Czy dodać kolejny składnik [y]")
     for i in range(len(components)):
         result += components[i]
     logging.debug(f"Dodaję następujące liczby {components}")
     print(f"Wynik to {result}")
 
 def multiply(variable1,variable2):
-    res=None
     components=[variable1,variable2]
-    i=3
+    counter=3
     result=1
     res=input("Czy pomnożyć kolejny składnik [y]")
     while res == 'y':
-        component=input(f"Podaj składnik {i}: ")
+        component=input(f"Podaj składnik {counter}: ")
         try:
-            float(component)
-            res=True
+            components.append(float(component))
         except:
             print("Podany składnik nie jest liczbą")
             exit()
-        components.append(float(component))
-        i+=1
-        res=input("Czy pomnożyć kolejny składnik [Y]")
+        counter+=1
+        res=input("Czy pomnożyć kolejny składnik [y]")
     for i in range(len(components)):
         result = result * components[i]
     logging.debug(f"Mnożę następujące liczby {components}")
