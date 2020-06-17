cf6c4cb3b3977fe861bf6fbb2a653e669f338d14
Dominik Baran <dominik.baran1992@gmail.com>
2020-06-17 13:57:01 +0200

    Make program updates

--- main.py
+++ main.py
@@ -2,30 +2,33 @@ import logging
 import operations
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filename="logfile.log")
 
+components=[]
+
 operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-variable1 = input("Podaj składnik 1. ")
 try:
-    variable1=float(variable1)
+    variable1=float(input("Podaj składnik 1. "))
 except:
     print("Podany składnik nie jest liczbą")
     exit()
 
-variable2 = input("Podaj składnik 2. ")
 try:
-    variable2=float(variable2)
+    variable2=float(input("Podaj składnik 2. "))
 except:
     print("Podany składnik nie jest liczbą")
     exit()
 
+components=[variable1,variable2]
 if operation==1:
-    operations.add(variable1,variable2)
+    operations.take_components(components)
+    logging.debug(f"Dodaję następujące liczby {components}")
+    print(f"Wynik to {operations.add(components)}")
 elif operation==2:
     logging.debug(f"Odejmuję {variable2} od {variable1}")
     print(f"Wynik to {variable1-variable2}")
 elif operation==3:
-    operations.multiply(variable1,variable2)
+    operations.take_components(components)
+    logging.debug(f"Mnożę następujące liczby {components}")
+    print(f"Wynik to {operations.multiply(components)}")
 elif operation==4:
     logging.debug(f"Dzielę {variable1} przez {variable2}")
     print(f"Wynik to {variable1/variable2}")
-
-
--- operations.py
+++ operations.py
@@ -1,9 +1,19 @@
 import logging
 
-def add(variable1,variable2):
-    components=[variable1,variable2]
-    counter=3
+def add(components):
     result=0
+    for i in range(len(components)):
+        result += components[i]
+    return result
+
+def multiply(components):
+    result=1
+    for i in range(len(components)):
+        result = result * components[i]
+    return result
+
+def take_components(components):
+    counter=3
     res=input("Czy dodać kolejny składnik [y]")
     while res == 'y':
         component=input(f"Podaj składnik {counter}: ")
@@ -14,26 +24,3 @@ def add(variable1,variable2):
             exit()
         counter+=1
         res=input("Czy dodać kolejny składnik [y]")
-    for i in range(len(components)):
-        result += components[i]
-    logging.debug(f"Dodaję następujące liczby {components}")
-    print(f"Wynik to {result}")
-
-def multiply(variable1,variable2):
-    components=[variable1,variable2]
-    counter=3
-    result=1
-    res=input("Czy pomnożyć kolejny składnik [y]")
-    while res == 'y':
-        component=input(f"Podaj składnik {counter}: ")
-        try:
-            components.append(float(component))
-        except:
-            print("Podany składnik nie jest liczbą")
-            exit()
-        counter+=1
-        res=input("Czy pomnożyć kolejny składnik [y]")
-    for i in range(len(components)):
-        result = result * components[i]
-    logging.debug(f"Mnożę następujące liczby {components}")
-    print(f"Wynik to {result}")
